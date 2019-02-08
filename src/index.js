7de3abb58bd0c3b1eec6a0288afbaea9ca0b9f4e
Leonardo Pereira <lpsilva1@stefanini.com>
2019-02-08 16:14:41 -0200

    Inplementação de cors e socket

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 mongoose.connect('URL', {
   useNewUrlParser: true,
 });
 
+app.use((request, response, next) => {
+  request.io = io;
+
+  return next();
+});
+
+app.use(cors());
 app.use(express.json());
 app.use(require('./routes'));
 
