708424ea7e3223957322420128afa333eb4c6a5b
Leonardo Pereira <lpsilva1@stefanini.com>
2019-02-08 15:58:37 -0200

    criação de likeController

---
+++ src/controllers/LikeController.js
@@ -0,0 +1,10 @@
+const Tweet = require('../models/Tweet');
+
+module.exports = {
+  async store(request, response) {
+    const tweet = await Tweet.findById(request.params.id);
+    tweet.set({ likes: tweet.likes + 1 });
+    await tweet.save();
+    return response.json(tweet);
+  },
+};
